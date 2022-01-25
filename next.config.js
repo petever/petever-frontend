1323fd66e73143ce50ee209243a423708537abd0
김승훈 <rlatmdgns94@naver.com>
2022-01-26 01:07:25 +0900

    fix: ignore pageName 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
   webpack(config) {
+    const prod = process.env.NODE_ENV === 'production';
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
 
-    return config;
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+    };
   },
-};
+});
